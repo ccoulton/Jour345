e6b261da70f136026261aa067b28bd41760af4b8
Charles Coulton <ccoulton@nevada.unr.edu>
2015-08-30 16:22:21 -0700

    roll a ball inital push

---
+++ Roll the ball/Assets/scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	float speed;
+	Rigidbody rigidBody;
+	private int count;
+	public Text scorecounter;
+	public Text winMessage;
+
+	void Start(){
+		speed = 11;
+		rigidBody = GetComponent <Rigidbody> ();
+		count = 0;
+		setScoreText ();
+		winMessage.text = "";
+	}
+	
+	void Update(){
+		
+	}
+	
+	void FixedUpdate(){
+		float moveHorz = Input.GetAxis ("Horizontal");
+		float moveVert = Input.GetAxis ("Vertical");
+		Vector3 moveVect = new Vector3 (moveHorz, 0, moveVert);
+		rigidBody.AddForce (moveVect*speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Can")) {
+			other.gameObject.SetActive(false);
+			count += 1;
+			setScoreText();
+		}
+	}
+
+	void setScoreText(){
+		scorecounter.text = "Score: " + count.ToString ();
+		if (count > 11) {
+			winMessage.text = "You got all the bug butt JUICE!\nYou Win?";
+		}
+	}
+}
